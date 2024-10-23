15433789cd95ce036ed29b426ad873987e410d06
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-23 11:03:27 -0300

    feat: storybook config

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  staticDirs: ['../public'],
+  stories: ['../src/components/**/stories.tsx'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+}
+export default config
