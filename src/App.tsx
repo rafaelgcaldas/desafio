d13849b6d229179f3b474567efd721b2332adf12
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-23 11:17:27 -0300

    feat: add tailwindcss config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
+import './styles/global.css'
+
 export function App() {
   return <RouterProvider router={router} />
 }
