6844a66248d2325d964c618693e24efa8104300f
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-23 16:37:04 -0300

    feat: add shdcn-ui config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,15 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+import path from 'path'
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
