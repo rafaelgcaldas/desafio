459f761755232924132ab5f60c1aa27d522493c4
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-23 16:42:51 -0300

    feat: add header story (storybook)

---
+++ src/components/header/stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Header } from '.'
+
+export default {
+  title: 'Header',
+  component: Header,
+  parameters: {
+    layout: 'full',
+  },
+} as Meta
+
+export const Default: StoryObj = {}
