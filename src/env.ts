03f90d73fb3fc98c21cf072f3ff9889907f3030e
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-24 13:55:03 -0300

    feat: add api keys

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
+  VITE_API_MARVEL_PUBLIC_KEY: z.string(),
+  VITE_API_MARVEL_PRIVATE_KEY: z.string(),
 })
 
 export const env = envSchema.parse(import.meta.env)
