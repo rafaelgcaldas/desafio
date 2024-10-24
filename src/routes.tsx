3b6042d8cba2a198c510783b58b5bd67175ff043
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-24 13:56:14 -0300

    feat: add characterId in details route

--- src/routes.tsx
+++ src/routes.tsx
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: '/details',
+        path: '/details/:characterId',
         element: <CharacterDetails />,
       },
     ],
