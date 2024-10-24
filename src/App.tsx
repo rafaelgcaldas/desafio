a4af6c1f4e0456f1268aa0bd08fca6465529d8a1
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-24 09:06:44 -0300

    feat: add tanstack react query config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 import './styles/global.css'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
 }
