343050a9ae0fb05e35a4a08d949af763d69bc0cd
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-25 10:36:12 -0300

    feat: add searchDialog storie (storybook)

---
+++ src/components/search-dialog/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SearchDialog } from '.'
+
+export default {
+  title: 'SearchDialog',
+  component: SearchDialog,
+} as Meta
+
+export const Default: StoryObj = {}
