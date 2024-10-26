c3929755ba4d3064e7817e00c63674f584ad3853
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-26 01:04:59 -0300

    test: add footer test

---
+++ src/components/footer/footer.spec.tsx
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom'
+
+import { describe, expect, it } from 'vitest'
+import { Footer } from '.'
+import { render } from '@testing-library/react'
+
+describe('<Footer />', () => {
+  it('should be able to render correctly', () => {
+    const container = render(<Footer />)
+
+    expect(container.getByRole('img', { name: 'Marvel logo' })).toBeVisible()
+    expect(container.getByText('Data provided by Marvel. © 2024 MARVEL'))
+  })
+})
