fe38af4e31d2ef7e492a51831c1877f05f92aeab
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-26 01:09:39 -0300

    fix: fix responsiveness

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -7,7 +7,7 @@ type BannerProps = {
 
 export function Banner({ title, hasLogo = false }: BannerProps) {
   return (
-    <div className="flex h-[400px] flex-col items-center justify-center gap-6 bg-banner-marvel bg-bottom p-4">
+    <div className="flex h-[400px] flex-col items-center gap-6 bg-banner-marvel bg-bottom p-4 sm:justify-center">
       <h1 className="text-center text-3xl font-semibold tracking-tight text-white md:text-5xl">
         {title}
       </h1>
