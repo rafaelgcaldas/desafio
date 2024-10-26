c56d91028ad9bfea7aa91f104e32894241617da7
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-26 01:05:37 -0300

    feat: add footer storie (storybook)

---
+++ src/components/footer/stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Footer } from '.'
+
+export default {
+  title: 'Footer',
+  component: Footer,
+  parameters: {
+    layout: 'full',
+  },
+} as Meta
+
+export const Default: StoryObj = {}
