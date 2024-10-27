f44a2c27dda27baf1c9ff54d270962e4c407fdca
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-26 21:45:43 -0300

    feat: add skeleton list storie (storybook)

---
+++ src/components/loading/skeleton-list/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SkeletonList } from '.'
+
+export default {
+  title: 'Skeleton list',
+  component: SkeletonList,
+} as Meta
+
+export const Default: StoryObj = {}
