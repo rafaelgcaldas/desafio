462508408ee33db2560853d86bfdb456e1578f77
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-26 21:44:09 -0300

    feat: add skeleton card storie (storybook)

---
+++ src/components/loading/card/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SkeletonCard } from '.'
+
+export default {
+  title: 'Skeleton Card',
+  component: SkeletonCard,
+} as Meta
+
+export const Default: StoryObj = {}
