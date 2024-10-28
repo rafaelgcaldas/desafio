65d260d8e5f529ae611a1b941ffaa211f0dac907
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-28 15:34:55 -0300

    fix: footer test

--- src/components/footer/footer.spec.tsx
+++ src/components/footer/footer.spec.tsx
@@ -8,7 +8,9 @@ describe('<Footer />', () => {
   it('should be able to render correctly', () => {
     const container = render(<Footer />)
 
-    expect(container.getByRole('img', { name: 'Marvel logo' })).toBeVisible()
+    expect(
+      container.getByRole('img', { name: 'Marvel logo footer' })
+    ).toBeVisible()
     expect(container.getByText('Data provided by Marvel. © 2024 MARVEL'))
   })
 })
