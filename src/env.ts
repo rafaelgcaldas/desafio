7954b1e7ebb6b96e4fcdef4e54f6a94d7f5d52f3
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-28 15:03:09 -0300

    feat: config msw

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  VITE_API_URL: z.string().url(),
+  MODE: z.enum(['production', 'development', 'test']),
+  VITE_API_URL: z.string(),
   VITE_API_MARVEL_PUBLIC_KEY: z.string(),
   VITE_API_MARVEL_PRIVATE_KEY: z.string(),
 })
