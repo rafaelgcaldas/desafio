a2a4e3e3aad9ea7a38f4380c9e149f30c14c96a3
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-10-28 22:11:04 -0300

    refactor: refactor home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,33 @@
 import { getCharacters } from '@/api/get-characters'
 import { Banner } from '@/components/banner'
+import { Card } from '@/components/card'
+import { SkeletonList } from '@/components/loading/skeleton-list'
 import { Pagination } from '@/components/pagination'
+import { Wrapper } from '@/components/wrapper'
+import { useStore } from '@/store'
+import type { Characters } from '@/types/types'
 import { useQuery } from '@tanstack/react-query'
-import { MoveUpRight, Star } from 'lucide-react'
-import { Link, useSearchParams } from 'react-router-dom'
-import { twMerge } from 'tailwind-merge'
+import { Info } from 'lucide-react'
+import { useEffect } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { z } from 'zod'
 
 export function Home() {
+  const {
+    characters,
+    favoritesList,
+    setCharacters,
+    addFavoriteCharacter,
+    addRecentlyVisitedCharacter,
+    recentlyVisitedCharactersList,
+  } = useStore()
+
+  const navigate = useNavigate()
+
+  const AllCharactersIsTop =
+    favoritesList.length === 0 && recentlyVisitedCharactersList?.length === 0
+  const recentlyVisited = favoritesList.length === 0
+
   const [searchParams, setSearchParams] = useSearchParams()
   const limit = 20
 
@@ -16,13 +36,24 @@ export function Home() {
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: characters } = useQuery({
+  const {
+    data: charactersData,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ['characters', pageIndex],
     queryFn: () => getCharacters({ pageIndex, limit }),
     staleTime: 5 * 10000,
     refetchOnWindowFocus: false,
     refetchOnMount: false,
+    retry: false,
   })
+
+  useEffect(() => {
+    if (charactersData) {
+      setCharacters(charactersData)
+    }
+  }, [charactersData, setCharacters])
 
   function handlePaginate(pageIndex: number) {
     setSearchParams((state) => {
@@ -32,94 +63,104 @@ export function Home() {
     })
   }
 
+  function handleCharacterNavigation(characterVisited: Characters) {
+    addRecentlyVisitedCharacter(characterVisited)
+    navigate(`/details/${characterVisited.id.toString()}`)
+  }
+
+  function handleAddFavoriteCharacter(character: Characters) {
+    const favoriteCharacter = {
+      ...character,
+      favorite: true,
+    }
+
+    addFavoriteCharacter(favoriteCharacter)
+  }
+
+  if (isError) {
+    return (
+      <>
+        <Banner title="Sua coleção de personagens" />
+
+        <div className="mx-auto max-w-[1200px] p-4">
+          <div className="flex items-center gap-4 rounded-lg bg-red-100 px-4 py-8">
+            <Info className="size-5" />
+            <p>
+              Ocorreu um erro ao carregar a lista de personagens. Atualize a
+              página e tente novamente.
+            </p>
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <>
+        <Banner hasLogo={true} title="Seja bem vindo ao universo" />
+
+        <Wrapper title=" Todos os personagens" isTop={AllCharactersIsTop}>
+          <SkeletonList />
+
+          {characters && (
+            <div className="mt-4">
+              <Pagination
+                onPageChange={handlePaginate}
+                pageIndex={pageIndex}
+                totalCount={characters.data.total}
+                perPage={characters.data.limit}
+              />
+            </div>
+          )}
+        </Wrapper>
+      </>
+    )
+  }
+
   return (
     <>
       <Banner hasLogo={true} title="Seja bem vindo ao universo" />
 
-      <div className="mx-auto -mt-32 max-w-[1200px] p-4">
-        <h2 className="mb-8 text-3xl font-semibold text-white">
-          Todos os personagens
-        </h2>
-
-        <div className="grid grid-cols-4 gap-4">
-          {characters?.data?.results?.map((character) => (
-            <div
-              key={character.id}
-              className="group max-w-sm rounded-lg border border-gray-200 bg-white hover:shadow-xl"
-            >
-              <img
-                className="aspect-square w-full rounded-t-lg"
-                src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                alt={character.name}
+      {favoritesList.length > 0 && (
+        <Wrapper title="Personagens favoritos">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {favoritesList.map((character) => (
+              <Card
+                key={character.id}
+                character={character}
+                onCharacterNavigation={handleCharacterNavigation}
+                onFavoriteNavigation={handleAddFavoriteCharacter}
               />
-              <div className="space-y-4 p-5">
-                <div className="flex items-center justify-between gap-2">
-                  <h5 className="mb-2 overflow-hidden truncate text-lg font-bold tracking-tight text-gray-900 transition group-hover:text-blue-500">
-                    {character.name}
-                  </h5>
-                  <Star
-                    className={twMerge(
-                      'size-5',
-                      character.favorite ? 'text-yellow-500' : ''
-                    )}
-                  />
-                </div>
+            ))}
+          </div>
+        </Wrapper>
+      )}
 
-                <div className="divide-y">
-                  <div className="flex items-center">
-                    <div className="min-w-0 flex-1">
-                      <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-                        Quadrinhos
-                      </p>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                      12
-                    </div>
-                  </div>
+      {recentlyVisitedCharactersList?.length > 0 && (
+        <Wrapper title="Visualizados recentemente" isTop={recentlyVisited}>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {recentlyVisitedCharactersList.map((character) => (
+              <Card
+                key={character.id}
+                character={character}
+                onCharacterNavigation={handleCharacterNavigation}
+                onFavoriteNavigation={handleAddFavoriteCharacter}
+              />
+            ))}
+          </div>
+        </Wrapper>
+      )}
 
-                  <div className="flex items-center">
-                    <div className="min-w-0 flex-1">
-                      <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-                        Séries
-                      </p>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                      8
-                    </div>
-                  </div>
-
-                  <div className="flex items-center">
-                    <div className="min-w-0 flex-1">
-                      <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-                        Histórias
-                      </p>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                      10
-                    </div>
-                  </div>
-
-                  <div className="flex items-center">
-                    <div className="min-w-0 flex-1">
-                      <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-                        Eventos
-                      </p>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                      0
-                    </div>
-                  </div>
-                </div>
-
-                <Link
-                  to={`/details/${character.id}`}
-                  className="inline-flex w-full items-center justify-center gap-2 rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  VER MAIS
-                  <MoveUpRight className="size-4" />
-                </Link>
-              </div>
-            </div>
+      <Wrapper title=" Todos os personagens" isTop={AllCharactersIsTop}>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {characters?.data?.results?.map((character) => (
+            <Card
+              key={character.id}
+              character={character}
+              onCharacterNavigation={handleCharacterNavigation}
+              onFavoriteNavigation={handleAddFavoriteCharacter}
+            />
           ))}
         </div>
 
@@ -133,7 +174,7 @@ export function Home() {
             />
           </div>
         )}
-      </div>
+      </Wrapper>
     </>
   )
 }
